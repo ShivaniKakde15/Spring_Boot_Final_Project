00ef39f63ded1bc57ad7b5242e120cb6defe448e
ShivaniKakde15 <kalyanikakade99@gmail.com>
2023-07-22 15:30:47 +0530

    chng

--- src/main/java/com/example/final_project/Repository/finalprojectRepo.java
+++ src/main/java/com/example/final_project/Repository/finalprojectRepo.java
@@ -3,11 +3,11 @@ package com.example.final_project.Repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.stereotype.Repository;
+
 
 import com.example.final_project.Model.User;
 
-// @Repository
+
 public interface finalprojectRepo extends JpaRepository<User, Integer>{
     Optional<User> findByUsernameAndPassword(String username, String password);
     Optional<User> findFirstByUsername(String username);
