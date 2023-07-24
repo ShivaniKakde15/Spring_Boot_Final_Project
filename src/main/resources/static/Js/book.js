1d0feefeaa5f78a07069e93491c0d99df7b7e21b
ShivaniKakde15 <kalyanikakade99@gmail.com>
2023-07-24 20:51:06 +0530

    save chng

--- src/main/resources/static/Js/book.js
+++ src/main/resources/static/Js/book.js
@@ -7,7 +7,7 @@ function myFunction() {
 // disable old date
 var date = new Date();    //display todays date(mm-dd-yy)
 var tdate = date.getDate();   //show todays date only (8)
-var month = date.getMonth() + 1;  //return last month (6)
+var month = date.getMonth() + 1;  //(getMonth() method returns value between 0 and 11) return last month (6) if add one(6+1) then return current month
 if (tdate < 10) {
   tdate = "0" + tdate; //(08)
 }
@@ -17,7 +17,7 @@ if (month < 10) {
 var year = date.getUTCFullYear();  //returns current yr
 var minDate = year + "-" + month + "-" + tdate;
 document.getElementById("disable").setAttribute('min', minDate)
-console.log(minDate);
+console.log(minDate); //2023-07-08
 
 
 
