3d0c5a317b02d9740651fa10719ce98260c469f2
ShivaniKakde15 <kalyanikakade99@gmail.com>
2023-07-25 09:47:43 +0530

    save changes

--- src/main/java/com/example/final_project/Repository/finalprojectRepo.java
+++ src/main/java/com/example/final_project/Repository/finalprojectRepo.java
@@ -8,8 +8,8 @@ import com.example.final_project.Model.User;
 
 
 public interface finalprojectRepo extends JpaRepository<User, Integer>{
-    Optional<User> findByUsernameAndPassword(String username, String password);
-    Optional<User> findFirstByUsername(String username);
+    Optional<User> findByUsernameAndPassword(String username, String password); //findByUsernameAndPassword method to handle user login functionality
+    Optional<User> findFirstByUsername(String username);   //findFirstByUsername method to find a user by their username.
     
 }
 
